db683d5c4cb75c2c5238792c2517d12ce029fd04
Lucashylario <lucashyfrotend@hotmail.com>
2022-06-22 16:20:42 -0300

    created lesson and sidebar

---
+++ event-platform/src/components/Sidebar.tsx
@@ -0,0 +1,53 @@
+import { gql, useQuery } from "@apollo/client";
+import { Lesson } from "./Lesson";
+
+//Import info do GhrapCMS
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC) {
+      id
+      lessonType
+      availableAt
+      title
+      slug
+    }
+  }
+`
+
+//Definição de tipagem para query lessons
+interface GetLessonsQueryResponse {
+  lessons: {
+    id: string
+    title: string
+    slug: string
+    availableAt: string
+    lessonType: 'live' | 'class'
+  }[] // colchetes no final do objeto é para dizer que retorna um array de objetos
+}
+
+export function Sidebar() {
+    const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
+
+    return (
+      <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+        <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+          Cronogramas de aulas
+        </span>
+
+        <div className="flex flex-col gap-8">
+          {data?.lessons.map(lesson => {
+            return (
+              <Lesson 
+                key={lesson.id}
+                title={lesson.title}
+                slug={lesson.slug}
+                availableAt={new Date(lesson.availableAt)}
+                type={lesson.lessonType}
+              />
+            )
+          })}
+        </div>
+      </aside>
+    )
+  }
+  
