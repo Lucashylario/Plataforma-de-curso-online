8fbd24f22fc2f3b8f512e91686b85687ee1d2c36
Lucashylario <lucashyfrotend@hotmail.com>
2022-06-23 18:20:57 -0300

    fix cache apollo

--- event-platform/src/lib/apollo.ts
+++ event-platform/src/lib/apollo.ts
@@ -1,6 +1,22 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
+
+
+const defaultOptions: DefaultOptions = {
+
+  watchQuery: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "ignore",
+  },
+
+  query: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "all",
+  },
+};
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oac0xw1ll601xi4krb2de9/master',
-    cache: new InMemoryCache()
-})
+  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oac0xw1ll601xi4krb2de9/master',
+
+  cache: new InMemoryCache(),
+  defaultOptions: defaultOptions,
+});
